7a71a4355805fbe418097303a296d021e7d96187
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-09-02 00:06:11 +0530

    chor: news fetching with axios instance

--- src/screns/BodySection.tsx
+++ src/screns/BodySection.tsx
@@ -1,20 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
+import instance from "../utils/AxiosInstance";
+
+type ArticleProps = {
+  source: object;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+};
 
 function BodySection() {
-    
-  useEffect(() => {
-    first;
+  const [responseData, SetResponseData] = useState<null | ArticleProps[]>(null);
+  const [isLoading, SetIsLoading] = useState(true);
+  const [isError, SetIsError] = useState(false);
 
-    return () => {
-      second;
-    };
-  }, [third]);
+  useEffect(() => {
+    instance
+      .get("&sortBy=popularity")
+      .then((response) => {
+        SetResponseData(response.data['articles']);
+        SetIsLoading(false);
+      })
+      .catch((error) => {
+        SetIsError(true);
+        console.log(error);
+      });
+  }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<HomeScreen />} />
+      <Route path="/" element={<HomeScreen responseData={responseData} />} />
     </Routes>
   );
 }
---
+++ src/utils/AxiosInstance.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `${import.meta.env.VITE_NEWS_APP_API_URL}`,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `${import.meta.env.VITE_NEWS_APP_API_KEY}`,
+  },
+});
+
+export default instance;
