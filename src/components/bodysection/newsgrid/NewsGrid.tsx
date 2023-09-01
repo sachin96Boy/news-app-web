2541cc7943437c3eebfd7f9d8a1de74c73d47f7a
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-09-02 01:34:14 +0530

    chor:newsGrid

--- src/components/bodysection/newsgrid/NewsGrid.tsx
+++ src/components/bodysection/newsgrid/NewsGrid.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState } from "react";
+import { ArticleProps } from "../../../screns/BodySection";
+import NewsCard from "../../newsCard/NewsCard";
 
-function NewsGrid() {
+const listTitles = ["Latest Stories", "Think", "Health"];
+
+function NewsGrid(props: ArticleProps[]) {
+  const [showMore, SetShowMore] = useState(false);
+
+  const handleClick = () => {
+    SetShowMore(!showMore);
+  };
+
+  const data = showMore ? props : props.slice(0, 4);
   return (
-    <div>NewsGrid</div>
-  )
+    <div className="flex flex-col items-center justify-between">
+      <div className="flex items-center justify-between bg-white h-12">
+        <ul className=" flex items-center gap-2 font-poppins font-semibold list-none">
+          {listTitles.map((item, index) => (
+            <li
+              key={index}
+              className={index == 0 ? "flex flex-col items-start" : undefined}
+            >
+              {item}
+              <div
+                className={
+                  index == 0
+                    ? "divide-solid divide-y-2 border-red-800"
+                    : undefined
+                }
+              ></div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {data.map((article, index) => (
+          <div key={index}>
+            <NewsCard
+              author={article.author}
+              description={article.description}
+              imgUrl={article.urlToImage}
+              time={article.publishedAt}
+              title={article.title}
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        className="border-red-400 border-2 text-red-800 font-poppins font-medium p-12"
+        onClick={handleClick}
+      >
+        VIEW MORE
+      </div>
+    </div>
+  );
 }
 
-export default NewsGrid
+export default NewsGrid;
