8ed408687e71b9d6f268fab0a468ac24e90100a7
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-09-02 14:22:00 +0530

    chor: key

--- src/utils/AxiosInstance.tsx
+++ src/utils/AxiosInstance.tsx
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: `${import.meta.env.VITE_NEWS_APP_API_URL}`,
   //   timeout: 1000,
   headers: {
-    // "Content-Type": "application/json",
+    "Content-Type": "application/json",
     Authorization: `${import.meta.env.VITE_NEWS_APP_API_KEY}`,
     "X-Api-Key": `${import.meta.env.VITE_NEWS_APP_API_KEY}`,
   },
