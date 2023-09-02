4605f7069aba660ca201e228f52983c628476bf1
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-09-02 13:51:09 +0530

    chor: min update

--- src/screns/ArticleScreen.tsx
+++ src/screns/ArticleScreen.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function ArticleScreen() {
-  return (
-    <div>ArticleScreen</div>
-  )
+  return <div>ArticleScreen</div>;
 }
 
-export default ArticleScreen
+export default ArticleScreen;
